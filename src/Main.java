73aa857f1c7172f278010880257ea5af34338312
KatePril <ket14pril14@gmail.com>
2023-05-26 21:41:49 +0300

    Add methods calls in main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
         System.out.println("Version 1.0");
+        double miles = 15;
+        double kms = 26;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+        double milesRes = convertKmsToMiles(kms);
+        System.out.println(milesRes + " miles in " + kms + " kms.");
     }
 
     private static double convertMilesToKms(double miles) {
